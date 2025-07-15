fd3d7e414a7a6c6284403928d95ff5646f1cfb9f
HacenFilali <hacen.filali@yahoo.com>
2025-07-15 19:49:20 +0200

    Delete codes/semaine1/journal/TP1.py

--- codes/semaine1/journal/TP1.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd # Importing Pandas
-import matplotlib.pyplot as plt # Importing matplotlib for visualisation
-df = pd.read_csv('../DATA/pokemon_data.csv')
-df.iloc[2, 4] # Here we can see the information from second row and fourth␣ ↪colum
-df.iloc[:5, :5] # Here is the information of first five rows with first five␣↪column
